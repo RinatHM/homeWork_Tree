65e353cbf2b2a97b62e1bb9405769e20aebdf43a
Ринат <rinatmuhamedyarov1967@gmail.com>
2024-06-02 09:06:40 +0500

    Добавлен функционал в файл FamilyTree

--- FamilyTree.java
+++ FamilyTree.java
@@ -1,42 +1,43 @@
-package src.tree;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class FamilyTree implements Serializable {
-    private List<Human> familyMembers;
+    private List<Human> people;
 
     public FamilyTree() {
-        familyMembers = new ArrayList<>();
+        this.people = new ArrayList<>();
     }
 
-    public void addMember(Human human) {
-        familyMembers.add(human);
+    public void addPerson(Human person) {
+        this.people.add(person);
     }
 
-    public List<Human> getFamilyMembers() {
-        return familyMembers;
+    public void removePerson(int id) {
+        this.people.removeIf(person -> person.getId() == id);
     }
 
-    public void saveToFile(String filename) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-            oos.writeObject(this);
+    public Human getPersonById(int id) {
+        return this.people.stream().filter(person -> person.getId() == id).findFirst().orElse(null);
+    }
+
+    public List<Human> getPeople() {
+        return this.people;
+    }
+
+    public int getNextId() {
+        return this.people.size() + 1;
+    }
+
+    public void sortByName() {
+        this.people.sort(Comparator.comparing(Human::getName));
+    }
+
+    public void sortByAge() {
+        this.people.sort(Comparator.comparingInt(Human::getBirthYear));
+    }
+
+    public void printFamilyTree() {
+        for (Human person : people) {
+            System.out.println(person);
         }
-    }
-
-    public static FamilyTree loadFromFile(String filename) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            return (FamilyTree) ois.readObject();
-        }
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (Human member : familyMembers) {
-            sb.append(member).append("\n");
-        }
-        return sb.toString();
     }
 }
